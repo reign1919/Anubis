870b9b0559b18f8ba073be35d19b16b696757872
Reign Genesis <153378833+reign1919@users.noreply.github.com>
2024-06-15 16:26:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,33 @@
 // service-worker.js
 self.addEventListener('notificationclick', event => {
-    event.waitUntil(
-        clients.matchAll().then(clients => {
-            if (clients.length) {
-                clients[0].focus();
-            } else {
-                clients.openWindow('/');
-            }
-        })
-    );
+    const notification = event.notification;
+    const action = event.action;
+
+    if (action === 'close') {
+        notification.close();
+    } else {
+        event.waitUntil(
+            clients.matchAll().then(clientsArr => {
+                const hasClient = clientsArr.some(client => client.visibilityState === 'visible');
+                if (hasClient) {
+                    clientsArr[0].focus();
+                } else {
+                    clients.openWindow('/');
+                }
+            })
+        );
+    }
+});
+
+
+self.addEventListener('periodicsync', event => {
+    if (event.tag === 'send-notification') {
+        event.waitUntil(
+            // Logic to send a notification
+            self.registration.showNotification('Scheduled Notification', {
+                body: 'This is a message from the periodic sync!',
+                icon: '/path/to/your/icon.png'
+            })
+        );
+    }
 });
